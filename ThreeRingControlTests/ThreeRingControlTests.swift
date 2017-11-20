ff61a1c20a45a566098b7124b1d9cdbc74396500
Marc SUN <marc@Marcs-iMac.local>
2017-11-20 20:13:53 +0800

    create ThreeRingControl program

--- ThreeRingControlTests/ThreeRingControlTests.swift
+++ ThreeRingControlTests/ThreeRingControlTests.swift
@@ -1,15 +1,30 @@
-//
-//  ThreeRingControlTests.swift
-//  ThreeRingControlTests
-//
-//  Created by Marc SUN on 11/20/17.
-//  Copyright © 2017 SUN. All rights reserved.
-//
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 import XCTest
+
 @testable import ThreeRingControl
 
-class ThreeRingControlTests: XCTestCase {
+class ThreeRingControlTest: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -21,16 +36,16 @@ class ThreeRingControlTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testThreeRingView() {
+        let threeRings = ThreeRingView()
+        DispatchQueue.main.async {
+            threeRings.innerRingValue = 1.0
+            threeRings.middleRingValue = 1.0
+            threeRings.outerRingValue = 1.0
         }
+        let _ = expectation(forNotification: NSNotification.Name(rawValue: RingCompletedNotification), object: nil, handler: nil)
+        let _ = expectation(forNotification: NSNotification.Name(rawValue: AllRingsCompletedNotification), object: nil, handler: nil)
+        waitForExpectations(timeout: 0.1, handler: nil)
     }
-    
 }
+
